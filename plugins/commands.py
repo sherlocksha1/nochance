c4c065eece87c5ffd9164f457866d3b922a68a81
Shanid TG <109721076+Shazard10@users.noreply.github.com>
2023-02-27 08:41:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,7 +42,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('തൊടരുത്🚫', callback_data='start')
+            InlineKeyboardButton('തൊടരുത്', callback_data='start')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         m=await message.reply_sticker("CAACAgUAAxkBAAEHx69j73DLHENudXkKNFr1Yos5KzLCtgACKQEAAs5h_j96V8_7r1q-YS4E") 
@@ -85,7 +85,7 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('തൊടരുത്🚫', callback_data='start')
+            InlineKeyboardButton('തൊടരുത്', callback_data='start')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
